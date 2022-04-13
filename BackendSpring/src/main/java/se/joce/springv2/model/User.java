2dc85fe8bcba41b14d5eff5dbc6649d2bc6a5c25
josy010 <jjosefin.andersson@hotmail.com>
2022-04-13 13:58:20 +0200

    Adding entity user

---
+++ BackendSpring/src/main/java/se/joce/springv2/model/User.java
@@ -0,0 +1,31 @@
+package se.joce.springv2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(name = "unique_user_email", columnNames = { "email" })})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+}
