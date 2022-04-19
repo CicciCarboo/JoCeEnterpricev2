2324c0a2b55d37b7d6e72f4b5e035fe8824b3c35
CicciCarboo <72078492+CicciCarboo@users.noreply.github.com>
2022-04-19 10:12:33 +0200

    UserRole permissions

--- BackendSpring/src/main/java/se/joce/springv2/security/UserPermission.java
+++ BackendSpring/src/main/java/se/joce/springv2/security/UserPermission.java
@@ -1,4 +1,20 @@
 package se.joce.springv2.security;
 
 public enum UserPermission {
+
+    USER_READ("user:read"),
+    USER_WRITE("user:write"),
+    ADMIN_READ("admin:read"),
+    ADMIN_WRITE("user:write");
+
+    private final String permission;
+
+    //TODO varför är inte construktorn public?/C
+    UserPermission(String permission){
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
 }
